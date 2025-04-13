7f6d4921ba72d4d04773d2928345666c76128dc2
akcent <akcent0711@icloud.com>
2025-04-13 10:19:00 +0300

    add gitignore

--- obj/Debug/net9.0/ease_intro_api.AssemblyInfo.cs
+++ obj/Debug/net9.0/ease_intro_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ease_intro_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de483069585ac804615105d582e0106aa7413fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b319998d4dfc0817f1c93ee8fe458809356da7e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ease_intro_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ease_intro_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
