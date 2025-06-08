e6e12c15dd20a2292c78be43fc672742ddb4dea5
akcent <akcent0711@icloud.com>
2025-06-08 12:11:16 +0300

    update

--- obj/Debug/net9.0/ease_intro_api.AssemblyInfo.cs
+++ obj/Debug/net9.0/ease_intro_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ease_intro_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d9fc18b94eb77b72d0dff9cced900b46746a85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f8fb657d84e6ba0b7c721a7018ff87c5115dfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ease_intro_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ease_intro_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
